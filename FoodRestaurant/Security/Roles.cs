015259b3dfc660764afe66484170d5dd473bb42d
mukes <mukes@LAPTOP-F5R0JL99>
2025-03-23 13:23:00 +0530

    Add project files.

---
+++ FoodRestaurant/Security/Roles.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FoodRestaurant.Security
+{
+    public class Roles
+    {
+        public static async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            string[] roleNames = { "Admin", "Staff" };
+            IdentityResult roleResult;
+
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    roleResult = await roleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+        }
+    }
+}
